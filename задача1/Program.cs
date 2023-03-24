87b7a919ce0a131f671ec41f6d0275fb0192f859
ALEKSANDR <pavlenkoaleksandr270397@gmail.com>
2023-03-24 11:32:57 +0500

    второй метод

--- задача1/Program.cs
+++ задача1/Program.cs
@@ -13,3 +13,5 @@ int number;
 number = int.Parse(Console.ReadLine());
 int sqr = number*number;
 Console.WriteLine($"Квадрат числа {number} равен {sqr}");
+int sqr1 = Convert.ToInt32(Math.Pow(number, 3));
+Console.WriteLine($"Квадрат числа {number} равен {sqr1}");
