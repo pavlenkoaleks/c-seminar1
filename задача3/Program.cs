2772f196dd4f4a3bbd9ce49111694af808333187
ALEKSANDR <pavlenkoaleksandr270397@gmail.com>
2023-03-24 12:08:38 +0500

    задача3

---
+++ задача3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите число номер дня недели: ");
+int dennedeli;
+dennedeli = int.Parse(Console.ReadLine());
+
+
+if (dennedeli == 1){Console.WriteLine("понедльник");}
+if (dennedeli == 2){Console.WriteLine("вторник");}
+if (dennedeli == 3){Console.WriteLine("среда");}
+if (dennedeli == 4){Console.WriteLine("четверг");}
+if (dennedeli == 5){Console.WriteLine("пятница");}
+if (dennedeli == 6){Console.WriteLine("суббота");}
+if (dennedeli == 7){Console.WriteLine("воскресенье");}
