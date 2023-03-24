03545fb8d8e727a0f06c03659134a00d90a8b8c9
ALEKSANDR <pavlenkoaleksandr270397@gmail.com>
2023-03-24 11:28:03 +0500

    задача 1

---
+++ задача1/Program.cs
@@ -0,0 +1,15 @@
+﻿/***Задача 0:** 
+**Условие:**
+Напишите программу, которая на вход принимает число и 
+выдаёт его квадрат (число умноженное на само себя).
+**Например:**
+4 -> 16
+-3 -> 9
+-7 -> 49*/
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number;
+number = int.Parse(Console.ReadLine());
+int sqr = number*number;
+Console.WriteLine($"Квадрат числа {number} равен {sqr}");
