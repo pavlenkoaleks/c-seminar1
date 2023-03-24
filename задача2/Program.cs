65fce8a76e2396268a29097d483fd6a73f8ba13f
ALEKSANDR <pavlenkoaleksandr270397@gmail.com>
2023-03-24 11:51:19 +0500

    задача2

---
+++ задача2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите число 1: ");
+int number1;
+number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int number2;
+number2 = int.Parse(Console.ReadLine());
+
+if (number1 == number2*number2){Console.WriteLine("первое число - квадрат второго");}
+else {Console.WriteLine("первое число - не квадрат второго");}
