4dcedc6304a3ed5f7f8ebb5fdfc1a530034118dc
ALEKSANDR <pavlenkoaleksandr270397@gmail.com>
2023-03-27 15:24:47 +0500

    11

---
+++ задача11/Program.cs
@@ -0,0 +1,9 @@
+﻿Random random = new Random();
+int number = random.Next(1,1000);
+Console.WriteLine(number);
+
+int number1 = number / 100;
+int number3 = number % 10;
+int result = number1*10+number3;
+
+Console.WriteLine(result);
